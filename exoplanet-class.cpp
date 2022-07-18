5db80e0565e3ba57152e0cb1fd8d7de00a0ad330
Simone Christen <63757723+Sunnfast@users.noreply.github.com>
2022-07-17 23:09:46 -0700

    added cpp file

---
+++ exoplanet-class.cpp
@@ -0,0 +1,111 @@
+// Exoplanet Class by Simone Christen
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Exoplanet {
+
+  private:
+  string _name;
+  double _lightYears;
+  double _planetMass;
+  double _stellarMagnitude;
+  int _discoDate;
+
+  public:
+  void SetName(string);
+  void SetLightYears(double);
+  void SetPlanetMass(double);
+  void SetStellarMagnitude(double);
+  void SetDiscoDate(int);
+
+  string GetName();
+  double GetLightYears();
+  double GetPlanetMass();
+  double GetStellarMagnitude();
+  int GetDiscoDate();
+};
+
+// set function definitions
+
+void Exoplanet::SetName(string name) {
+  _name = name;
+}
+
+void Exoplanet::SetLightYears(double lightYears) {
+  _lightYears = lightYears;
+}
+
+void Exoplanet::SetPlanetMass(double planetMass) {
+  _planetMass = planetMass;
+}
+
+void Exoplanet::SetStellarMagnitude(double stellarMagnitude) {
+  _stellarMagnitude = stellarMagnitude;
+}
+
+void Exoplanet::SetDiscoDate(int discoDate) {
+  _discoDate = discoDate;
+}
+
+// get function definitions
+string Exoplanet::GetName() {
+  return _name;
+}
+
+double Exoplanet::GetLightYears() {
+  return _lightYears;
+}
+
+double Exoplanet::GetPlanetMass() {
+  return _planetMass;
+}
+
+double Exoplanet::GetStellarMagnitude() {
+  return _stellarMagnitude;
+}
+
+int Exoplanet::GetDiscoDate() {
+  return _discoDate;
+}
+
+
+int main() {
+
+  Exoplanet ComaeBerenices;
+  ComaeBerenices.SetName("11 Comae Berenices b");
+  ComaeBerenices.SetLightYears(304);
+  ComaeBerenices.SetPlanetMass(19.4);
+  ComaeBerenices.SetStellarMagnitude(4.72307);
+  ComaeBerenices.SetDiscoDate(2007);
+
+  Exoplanet UrsaeMinoris;
+  UrsaeMinoris.SetName("11 Ursae Minoris b");
+  UrsaeMinoris.SetLightYears(409);
+  UrsaeMinoris.SetPlanetMass(14.74);
+  UrsaeMinoris.SetStellarMagnitude(5.013);
+  UrsaeMinoris.SetDiscoDate(2009);
+
+  Exoplanet Andromedae;
+  Andromedae.SetName("14 Andromedae b");
+  Andromedae.SetLightYears(246);
+  Andromedae.SetDiscoDate(4.8);
+  Andromedae.SetStellarMagnitude(5.23133);
+  Andromedae.SetDiscoDate(2008);
+
+  Exoplanet Herculis;
+  Herculis.SetName("14 Herculis b");
+  Herculis.SetLightYears(58);
+  Herculis.SetPlanetMass(4.66);
+  Herculis.SetStellarMagnitude(6.61935);
+  Herculis.SetDiscoDate(2002);
+
+  Exoplanet Cygni;
+  Cygni.SetName("16 Cygni B b");
+  Cygni.SetLightYears(69);
+  Cygni.SetPlanetMass(1.78);
+  Cygni.SetStellarMagnitude(6.215);
+  Cygni.SetDiscoDate(1996);
+
+}
